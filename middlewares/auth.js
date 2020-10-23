ebf83387f1e72bb76d5eee5f85e69639d9ef0074
Sergey Denisenko <sd.a2@yandex.ru>
2020-10-23 18:42:53 +0300

    ПР15_merge front and back 3 correct Bearer

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,12 +9,12 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   console.log('authorization in Auth.js back');
   console.log(authorization);
-  if (!authorization && !authorization.startsWith('Bearer')) {
+  if (!authorization && !authorization.startsWith('Bearer ')) {
     // return res.status(401).send({ message: 'Необходима авторизация 1' });
     next(new AuthError('Unauthorized / Необходима авторизация 1')); // 401
   }
 
-  const token = authorization.replace('Bearer', '');
+  const token = authorization.replace('Bearer ', '');
   console.log('token in Auth.js back');
   console.log(token);
   let payload;
